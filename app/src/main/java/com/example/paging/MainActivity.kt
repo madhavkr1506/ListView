4bff4590ca9bb3f1799286e706045b85d5b72568
madhavkr1506 <madhavkr9153276724@gmail.com>
2025-01-26 13:50:47 +0530

    ListView

--- app/src/main/java/com/example/paging/MainActivity.kt
+++ app/src/main/java/com/example/paging/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.paging
 
+import android.media.MediaCodec.QueueRequest
 import android.os.Bundle
 import android.view.View
 import android.widget.AbsListView
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
